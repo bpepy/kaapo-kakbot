8436332c5cdf424dd5d67e81657eef986193b5c5
bpepy <33704441+bpepy@users.noreply.github.com>
2019-07-10 21:19:01 -0400

    Create kaapo.js

---
+++ kaapo.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+
+function wordInString(s, word){
+  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+}
+
+//wordInString('did you, or did you not, get why?', 'you')
+
+var mess=message.content.toLowerCase();
+
+  if (mess.substring(0,3) === 'wow') {
+    	    message.channel.send('It's a nice city.');
+  	    }
+	else if ((wordInString(mess,'praise be')== 1)) {
+		message.channel.send('Praise be :praisebe:');
+  	    }
+            break;
+         }
+     }
+});
+
+client.login(process.env.BOT_TOKEN);
